2a7d04e3ccdde03969f40adfb9a496eb2baf42f6
MHkhan994 <94839405+MHkhan994@users.noreply.github.com>
2024-01-01 21:26:49 +0600

    resourses cards

--- components/Homepage/Home.jsx
+++ components/Homepage/Home.jsx
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import HomeTabs from "./HomeTabs";
 import Features from "./Features";
 import Resources from "./Resources";
+import HomeContent from "./HomeContent";
 
 const Home = () => {
   const [currentTab, setCurrentTab] = useState("features");
@@ -10,6 +11,7 @@ const Home = () => {
   return (
     <div className="bg-gray my-container min-h-screen lg:p-[30px] p-[20px]">
       <HomeTabs setCurrentTab={setCurrentTab} currentTab={currentTab} />
+
       <hr className="lg:my-8 md:my-5 hidden md:block" />
 
       <div className="hidden md:block">
@@ -23,31 +25,7 @@ const Home = () => {
       </div>
 
       {/* tabs and sidebars */}
-      <div className="grid lg:grid-cols-[2fr_1fr] md:grid-cols-2 grid-cols-1 lg:mt-[30px] mt-[20px] gap-[20px] lg:gap-0">
-        {/* left side tab */}
-        <div className="bg-white md:min-h-[530px] w-full lg:p-[30px] p-[20px] rounded-[10px] border">
-          <div className="md:hidden pb-[16px]">
-            <h3 className="text-base lg:text-xl font-bold">
-              Welcome to <span className="text-[#CC176A]">BiyeKorun</span>
-              Documentation.
-            </h3>
-            <p className="text-sm text-gray-500 pt-2">
-              Find user guides, developer guides and API references.
-            </p>
-          </div>
-          <h1 className="text-base lg:text-2xl font-bold leading-none pb-[15px]">
-            {currentTab === "features"
-              ? "Features"
-              : currentTab === "resources"
-              ? "Developer Resources"
-              : ""}
-          </h1>
-          <div>{currentTab === "features" ? <Features /> : <Resources />}</div>
-        </div>
-
-        {/* right side bar */}
-        <div className="bg-white lg:mx-[30px] lg:p-[30px] p-[20px] h-full lg:w-fit w-full rounded-[10px] border"></div>
-      </div>
+      <HomeContent currentTab={currentTab} />
     </div>
   );
 };
---
+++ components/Homepage/HomeContent.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import features from "../../public/features.json";
+import resources from "../../public/resources.json";
+import InfoCards from "./InfoCards";
+
+import homeStyles from "./Home.module.css";
+
+const HomeContent = ({currentTab}) => {
+  return (
+    <div className="grid lg:grid-cols-[2fr_1fr] md:grid-cols-2 grid-cols-1 lg:mt-[30px] mt-[20px] gap-[20px] lg:gap-0">
+      {/* left side tab */}
+      <div className="bg-white md:min-h-[530px] w-full lg:p-[30px] p-[20px] rounded-[10px] border">
+        <div className="md:hidden pb-[16px]">
+          <h3 className="text-base lg:text-xl font-bold">
+            Welcome to <span className="text-[#CC176A]">BiyeKorun</span>
+            Documentation.
+          </h3>
+          <p className="text-sm text-gray-500 pt-2">
+            Find user guides, developer guides and API references.
+          </p>
+        </div>
+        <h1 className="text-base lg:text-2xl font-bold leading-none pb-[15px]">
+          {currentTab === "features"
+            ? "Features"
+            : currentTab === "resources"
+            ? "Developer Resources"
+            : ""}
+        </h1>
+        <div>
+          {currentTab === "features" ? (
+            <div
+              className={`${homeStyles.tab_container} grid lg:grid-cols-2 grid-cols-1 justify-center gap-[20px] md:h-[412px] w-full tab-container pr-3 overflow-y-auto`}
+            >
+              {features &&
+                features.map((f) => <InfoCards key={f.id} item={f} />)}
+            </div>
+          ) : (
+            <div
+              className={`${homeStyles.tab_container} grid lg:grid-cols-2 grid-cols-1 justify-center gap-[20px] md:h-[412px] w-full tab-container pr-3 overflow-y-auto`}
+            >
+              {features &&
+                resources.map((f) => <InfoCards key={f.id} item={f} />)}
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* right side bar */}
+      <div className="bg-white lg:mx-[30px] lg:p-[30px] p-[20px] h-full lg:w-fit w-full rounded-[10px] border"></div>
+    </div>
+  );
+};
+
+export default HomeContent;
